b7417868450aed0b859b232d531cc3e88836f657
Nishu <nishangithub@gmail.com>
2025-08-10 21:54:20 +0530

    Update socket.js cors socket

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://axio-n.vercel.app"],
   },
 });
 
@@ -32,3 +32,4 @@ io.on("connection", (socket) => {
 });
 
 export { app, io, server };
+
